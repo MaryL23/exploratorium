405d939ff829fe680e6f725a1d92f8c9749cbb53
Mary Linderman <mltchwrt@yahoo.com>
2020-05-17 14:56:50 -0500

    added tree fractuals and placeholder files

---
+++ algorithms/fractaltrees2.js
@@ -0,0 +1,49 @@
+let angle = 0;
+let slider;
+
+function setup() {
+  createCanvas(400, 400);
+
+  slider = createSlider(0, TWO_PI, PI / 2, 0.01);
+
+  slider.position(10, 420);
+  frameRate(1);
+}
+
+function draw() {
+  background(220);
+  angle = slider.value();
+
+  strokeWeight(2);
+  stroke("green");
+
+  translate(200, height);
+  branch(100);
+}
+
+function branch(len) {
+  line(0, 0, 0, -len);
+
+  translate(0, -len); //  starts from 0 x but moves to top of last branch
+
+  if (len > 10) {
+    push(); //save translation on right
+
+    stroke("red");
+    rotate(angle);
+    branch(len * 0.67);
+    pop(); //retorses to location
+
+    push();
+    stroke("blue");
+    rotate(-angle);
+    branch(len * 0.67);
+    pop();
+
+    push();
+    stroke("yellow");
+    rotate(0);
+    branch(len * 0.67);
+    pop();
+  }
+}
